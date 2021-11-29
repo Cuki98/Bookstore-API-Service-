083af4fce68fa5abb746f50872385b793b97130d
Cuki <cabrera9806@gmail.com>
2021-11-29 17:35:49 -0500

    Revert "Update BookController.java"
    This reverts commit c3294023f0fb9a7167a1cc39393e1b57454b0608.

--- src/main/java/net/javaguides/restfulapitest/controller/BookController.java
+++ src/main/java/net/javaguides/restfulapitest/controller/BookController.java
@@ -1,25 +1,17 @@
 package net.javaguides.restfulapitest.controller;
 
 
+import net.javaguides.restfulapitest.exception.ResourceNotFoundException;
+import net.javaguides.restfulapitest.model.Book;
+import net.javaguides.restfulapitest.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import net.javaguides.restfulapitest.exception.ResourceNotFoundException;
-import net.javaguides.restfulapitest.model.Book;
-import net.javaguides.restfulapitest.repository.BookRepository;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -39,8 +31,6 @@ public class BookController {
 
 
     }
-
-	// Test comment
 
     //get book by id
     @GetMapping("/books/{isbn}")
