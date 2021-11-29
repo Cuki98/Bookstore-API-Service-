4136318d76b913f91c91b0612c003f2ea309d7bd
mlerette <81699627+mlerette@users.noreply.github.com>
2021-11-29 10:14:34 -0500

    Changes to User classes

--- src/main/java/net/javaguides/restfulapitest/repository/UserRepository.java
+++ src/main/java/net/javaguides/restfulapitest/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package net.javaguides.restfulapitest.repository;
 
 import net.javaguides.restfulapitest.model.User;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
+	 public User findUserByEmail(String email);
 }
